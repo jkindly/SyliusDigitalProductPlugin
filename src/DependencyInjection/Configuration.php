b24fb18e11b2b7d5973ec9ca030c5368c1855a12
kozupa.jakub@gmail.com <kozupa.jakub@gmail.com>
2025-08-10 11:22:56 +0200

    init

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SyliusHotpayPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * @psalm-suppress UnusedVariable
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('acme_sylius_example');
+        $rootNode = $treeBuilder->getRootNode();
+
+        return $treeBuilder;
+    }
+}
---
+++ tests/TestApplication/config/bundles.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    SyliusHotpayPlugin\SyliusHotpayPlugin::class => ['all' => true],
+];
